9982f6fac29c0e6720bdf96b62fd36294047f366
Elgun Guluzada <elgunpg@code.edu.az>
2022-06-06 03:24:43 +0400

    feat : new plugin productCarousel

--- assets/script/script.js
+++ assets/script/script.js
@@ -29,7 +29,6 @@ $(document).ready(function () {
 
 //  Carousel Section Start
 
-
 let index = 0,
     amount = 0,
     currTransl = [],
@@ -55,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementsByClassName("slide")[i].addEventListener("MSTransitionEnd", transitionCompleted, true);
     }
     // add the last item to the start so that translateX(-moveOffset) works (In case the first click is the previous button)
-    document.getElementById('carousel').insertBefore(document.getElementById('carousel').children[amount - 1], document.getElementById('carousel').children[0])
+    document.getElementById('carousel').insertBefore(document.getElementById('carousel').children[3], document.getElementById('carousel').children[0])
     // add click events to control arrows
     document.getElementById('prev').addEventListener('click', prev, true);
     document.getElementById('next').addEventListener('click', next, true);
@@ -104,3 +103,51 @@ function next() {
 }
 
 //  Carousel Section End
+
+
+// product carousel start 
+
+
+let items = document.querySelectorAll('#featurecntnr .carousel .carousel-item');
+items.forEach((el) => {
+    const minPerSlide = 5
+    let next = el.nextElementSibling
+    for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+$(document).ready(function () {
+    $('#featureCarousel').carousel({ interval: false });
+    $('#featureCarousel').carousel('pause');
+});
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+try {
+    fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' })).then(function (response) {
+        return true;
+    }).catch(function (e) {
+        var carbonScript = document.createElement("script");
+        carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CK7DKKQU&placement=wwwjqueryscriptnet";
+        carbonScript.id = "_carbonads_js";
+        document.getElementById("carbon-block").appendChild(carbonScript);
+    });
+} catch (error) {
+    console.log(error);
+}
